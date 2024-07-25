219176bd518c77f4daadd9f603bcc7c7bc4095e7
abhisheksuhag <abhisheksuhag1010@gmail.com>
2024-07-25 16:43:13 +0530

    learning zod

---
+++ HTTP-server-1/zodd.js
@@ -0,0 +1,20 @@
+const express= require("express");
+const zod= require("zod");
+const app = express();
+
+function validateInput(obj){
+   const schema= zod.object({
+    email: zod.string().email(),
+    password: zod.string().min(8),
+   }) 
+
+   const response = schema.safeParse(obj);
+   console.log(response);
+}
+
+validateInput({
+    email: "abc@gmail.com",
+    password: "systemhailadle"
+})
+
+app.listen(3000);
