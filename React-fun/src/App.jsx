8594a02d2551c569fee61c39ef6825960fdd2f0d
abhisheksuhag <abhisheksuhag1010@gmail.com>
2024-08-29 09:54:01 +0530

    playing

--- React-fun/src/App.jsx
+++ React-fun/src/App.jsx
@@ -1,24 +1,47 @@
-
-import {useState} from "react";
-
+import { useState } from "react";
 
 function App() {
-  const [count, setCount]= useState(0);
-  console.log(count);
-  console.log(setCount);
+  const [todos, setTodos] = useState([
+    {
+      title: "go to the gym",
+      description: "at 9Am",
+      completed: false,
+    },
+    {
+      title: "go to the store",
+      description: "at 10Am",
+      completed: true,
+    },
+  ]);
 
-
-
-  function onClickHandler (){
-    setCount(count + 1);
+  function addTodo() {
+    setTodos([
+      ...todos,
+      {
+        title: "go shoot",
+        description: "at 11Am",
+      },
+    ]);
   }
 
-
-  return(
+  return (
     <div>
-      <button onClick={onClickHandler}>Counter - {count}</button>
+      <button onClick={addTodo}>Add a random todo</button>
+      {todos.map((todo, index) => (
+        <Todo key={index} title={todo.title} description={todo.description} />
+      ))}
     </div>
-  )
+  );
+}
+
+// Define the Todo component
+function Todo({ title, description }) {
+  return (
+    <div>
+      <h3>{title}</h3>
+      <p>{description}</p>
+    </div>
+  );
 }
 
 export default App;
