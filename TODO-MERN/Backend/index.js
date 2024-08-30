4880e4e750d8e04943c3f42116bac6edb8a69030
abhisheksuhag <abhisheksuhag1010@gmail.com>
2024-08-30 14:48:26 +0530

    updated post function in index.js

--- TODO-MERN/Backend/index.js
+++ TODO-MERN/Backend/index.js
@@ -2,12 +2,47 @@
  // with express.json() middleware
 
  const express= require('express');
+const { createTodo, updatetodo } = require('./types');
+const createApplication = require('express/lib/express');
  const port=3000;
  const app= express();
 
-app.use(express.json());
- app.get("/todo", function(req, res){
-    res.send("hello world")
+ app.use(express.json());
+
+ app.post("/todo",async function(req, res){
+    const createPayload= req.body;
+    const parsedPayload = createTodo.safeParse(createPayload);
+    if(!parsedPayload.success){
+      res.status(411).json({msg:"You sent the wrong inputs",})
+      return;
+
+    }
+    await todo.create({
+      title: createPayload.title,
+      description: createPayload.description,
+    })
+
+    res.json({
+      msg:"Todo Created",
+    })
+ }) 
+
+ app.get("/todos", function( req, res){
+   
  })
+ 
+ app.put("/completed", function(req, res){
+   const createPayload= req.body;
+   const parsedPayload = updateTodo.safeParse(updatePayload);
+   if(!parsedPayload.success){
+     res.status(411).json({msg:"You sent the wrong inputs",})
+     return;
+   }
+})
+
+
+
+
+
 
  app.listen(port);
