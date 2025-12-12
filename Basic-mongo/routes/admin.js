281fb2646225fcb4694cb4793c7ee02d357351b8
abhisheksuhag <abhisheksuhag1010@gmail.com>
2025-12-12 14:57:18 +0530

    feat: basic user  courses backend

---
+++ Basic-mongo/routes/admin.js
@@ -0,0 +1,45 @@
+const { Router } = require("express");
+const adminMiddleware = require("../middleware/admin");
+const { Admin, Course } = require("../db");
+
+const router = Router();
+
+router.post("/signup",async (req, res)=>{
+    const username = req.body.username;
+    const password = req.body.password;
+
+    await Admin.create({
+        username: username,
+        password: password,
+    })
+    res.json({
+        message: "Admin created successfully"
+    })
+
+    
+
+});
+
+router.post("/courses", adminMiddleware,  async (req, res)=>{
+    const title = req.body.title;
+    const description = req.body.description;
+    const imageLink = req.body.imageLink;
+    const price = req.body.price;
+
+    const newCourse = await Admin.create({
+        title, description, imageLink , price
+    })
+    console.log(newCourse);
+    res.json({
+        message: "Course created successfully", courseId: newCourse._id 
+    })
+});
+
+router.get("/courses", adminMiddleware,  async (req, res)=>{
+    const response = await Course.find({});
+    res.json({
+        courses: response
+    })
+});
+
+module.exports = router;
---
+++ Basic-mongo/routes/user.js
@@ -0,0 +1,22 @@
+const { Router } = require("express");
+const userMiddleware = require("../middleware/user");
+const router = Router();
+
+router.post('/signup', (req, res)=>{
+
+});
+
+router.get("/courses", ( req, res)=>{
+
+});
+
+router.post("/courses/:courseId", userMiddleware, ( req, res)=>{
+
+})
+
+router.get("/purchasedCourses", userMiddleware,  ( req, res) =>{
+
+});
+
+
+module.exports = router;
