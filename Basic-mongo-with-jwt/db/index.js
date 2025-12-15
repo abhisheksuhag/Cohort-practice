54332c437b19131a03e2a8844b5d6cad7fd8997a
abhisheksuhag <abhisheksuhag1010@gmail.com>
2025-12-15 11:06:20 +0530

    feat: user with jwt auth

---
+++ Basic-mongo-with-jwt/db/index.js
@@ -0,0 +1,38 @@
+require('dotenv').config()
+const mongoose = require("mongoose");
+
+const connectionString= process.env.MONGODB_URI;
+
+
+mongoose.connect(connectionString);
+
+const AdminSchema = new mongoose.Schema({
+    username: String,
+    password: String,
+});
+
+const UserSchema = new mongoose.Schema({
+    username: String,
+    password: String,
+    purchasedCourses: [{
+        type: mongoose.Types.ObjectId,
+        ref: "Course",
+    }]
+});
+
+const CourseSchema = new mongoose.Schema({
+    title: String,
+    description: String,
+    imageLink: String,
+    price: Number,
+});
+
+
+const Admin = mongoose.model("Admin", AdminSchema);
+const User = mongoose.model("User", UserSchema);
+const Course = mongoose.model("Course", CourseSchema);
+
+module.exports = {
+    Admin, User, Course
+}
+
--- Basic-mongo-with-jwt/middleware/admin.js
+++ Basic-mongo-with-jwt/middleware/admin.js
@@ -6,6 +6,16 @@ function adminMiddleware ( req, res, next) {
     const token = req.headers.authorization;
     const words = token.split(" ");
     const jwtToken = words[1];
+    const decodedValue = jwt.verify(jwtToken, secret);
+    
+    if(decodedValue){
+        next();
+    }
+    else {
+        res.status(403).json({
+            msg: "You are not authenticated"
+        })
+    }
 }
 
-module.exports = adminMiddleware;
+module.exports = adminMiddleware;;
