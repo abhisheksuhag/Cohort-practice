da6b02998862a089a130db9424259b27c09f5984
abhisheksuhag <abhisheksuhag1010@gmail.com>
2026-01-14 07:01:36 +0530

    chore: body parser and url encoder

--- Basic-mongo-with-jwt/index.js
+++ Basic-mongo-with-jwt/index.js
@@ -6,7 +6,10 @@ const adminRouter = require("./routes/admin");
 const userRouter = require("./routes/user");
 
 const JWT_SECRET = "djfkjgkdfjhgksdf";
-app.use(bodyParser.json());
+app.use(bodyParser.json());  //  body parser is used to make the incoming body of HTTP request convert to json object.
+// app.use(express.json());  this also acts as a middleware for parsing JSON
+app.use(express.urlencoded({extended: true})); // build in middleware for parsing URL-encoded data
+
 app.use("/admin", adminRouter);
 app.use("/user", userRouter);
 const PORT = 3000;
